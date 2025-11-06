55e4d4a8e7dd5927ab478108655a7db52e790332
Geoff <gzelenka+github@gmail.com>
2025-11-06 00:42:55 -0500

    Get some rudimentatry wsb daily thread stats

---
+++ src/gzreddit_scraper/main.py
@@ -0,0 +1,64 @@
+import reddit
+import stonkset
+
+import re
+
+from collections import defaultdict
+from pprint import pprint
+
+
+class WSBDailyWatcher:
+    """Do some stats on the Daily Threads at WSB"""
+
+    def __init__(self):
+        self._ticker_pattern = re.compile(r'\b\$?[A-Za-z]{1,5}\b')
+        self._stonks = stonkset.StonkSet()
+        self._reddit = reddit.Reddit()
+
+        self._stats = {
+            "user_post_count": defaultdict(int),
+            "ticker_count": defaultdict(int),
+            "comments_processed": 0,
+            }
+
+    def clean_text(self, text):
+        text = re.sub(r'http\S+', '', text)  # remove links
+        text = re.sub(r'[^A-Za-z0-9$ .,!?\'"\-]+', ' ', text)
+        return text.strip()
+
+    def process_comment(self, comment):
+        self._stats["comments_processed"] += 1
+        if comment.author != None:
+            self._stats["user_post_count"][comment.author.name] += 1
+
+        comment_text = self.clean_text(comment.body)
+        tickers = self._ticker_pattern.findall(comment_text)
+        tickers = [ t for t in tickers if self._stonks.contains(t.strip("$")) ]
+        for t in tickers:
+            self._stats["ticker_count"][t] += 1
+
+    def get_daily_thread_stats(self):
+        stickies = self._reddit.get_stickied_posts("wallstreetbets","Your Moves")
+        print(stickies)
+
+        for sticky in stickies:
+            print(f"Getting stats on {sticky.title}")
+            sticky.comments.replace_more(limit=0) # this flattens the comment tree
+            for comment in sticky.comments.list():
+                self.process_comment(comment)
+                  
+            print(f"Processed {self._stats["comments_processed"]} comments")
+        
+        print(f"Comments from {len(self._stats["user_post_count"])} redditors") 
+        pprint(sorted(self._stats["user_post_count"].items(), key=lambda item: item[1], reverse=True))
+
+        print(f"Comments mentioned {len(self._stats["ticker_count"])} different tickers")
+        pprint(sorted(self._stats["ticker_count"].items(), key=lambda item: item[1], reverse=True))
+
+
+
+
+if __name__ == "__main__":
+    wsb = WSBDailyWatcher()
+    wsb.get_daily_thread_stats()
+    
--- src/gzreddit_scraper/reddit.py
+++ src/gzreddit_scraper/reddit.py
@@ -1,6 +1,8 @@
 import os
 import praw
+import time
 
+from datetime import datetime
 from dotenv import load_dotenv
 
 
@@ -12,6 +14,7 @@ from dotenv import load_dotenv
 
 class Reddit:
     def __init__(self):
+        """INIT YO SELF"""
         load_dotenv()
         required_vars = ['REDDIT_KEY', 'REDDIT_SECRET', 'USER_AGENT']
         missing = [var for var in required_vars if not os.getenv(var)]
@@ -24,14 +27,57 @@ class Reddit:
             user_agent=os.getenv('USER_AGENT')
         )
 
+        #some stats of what we've done
+        self.submissions = 0
+        self.comments = 0
+
     def get_test_posts(self):
+        """Get the last 10 posts from the "hot" sort of the "test" subreddit"""
         for p in self.reddit.subreddit("test").hot(limit=10):
             print(p.title)
+
+
+    def get_new_posts(self, subreddit, time_since = 0, max_posts=None):
+        """Get posts from a specified subreddit, can limit by number of posts or number of seconds back to go"""
+        sub = self.reddit.subreddit(subreddit)
+        posts = []
+
+        for submission in sub.new(limit=max_posts):
+            if submission.created_utc > time_since:
+                posts.append(submission)
+            else:
+                break
+
+        self.submissions += len(posts)
+        print(f"Found {len(posts)} since {datetime.fromtimestamp(time_since)}")
+
+        return posts
+
+    def get_stickied_posts(self, subreddit_name, title_filter=None):
+        """Get the pinned posts of a particular subreddit, with optional title filter"""
+        subreddit = self.reddit.subreddit(subreddit_name)
+
+        stickied_posts = []
+        try:
+            stickied_posts.append(subreddit.sticky(number=2))
+            if title_filter:
+                stickied_posts = [ post for post in stickied_posts if title_filter in post ]
+        except:
+            pass
+
+        return stickied_posts
+
+
 
 
 if __name__ == "__main__":
     r = Reddit()
 
-    r.get_test_posts()
+    ten_mins_ago = time.time() - (10 * 60)
 
+    new_posts = r.get_new_posts("politics",ten_mins_ago)
 
+    print(new_posts[0])
+
+    for p in new_posts:
+        print(f"{p.title} - {datetime.fromtimestamp(p.created_utc)}")
