af670a8a0ece4fd82cafd6a11b585f830c42111f
Geoff <gzelenka+github@gmail.com>
2025-11-05 22:32:09 -0500

    Add stonkset
    The StonkSet class encapsulates a set of stock tickers to be used
    elsewhere

---
+++ src/gzreddit_scraper/stonkset.py
@@ -0,0 +1,34 @@
+
+
+class StonkSet:
+    def __init__(self):
+        self._stonkset = set()
+    
+        ticker_files = [
+            "resources/nasdaq_tickers.txt",
+            "resources/nyse_tickers.txt"
+            ]
+        for tf in ticker_files:
+            with open(tf) as ticker_file:
+                for ticker in ticker_file.readlines():
+                    self._stonkset.add(ticker.rstrip())
+
+    def contains(self, stonk):
+        return stonk in self._stonkset
+
+
+    def size(self):
+        return len(self._stonkset)
+
+
+if __name__ == "__main__":
+    stonks = StonkSet()
+    print(f"StonkSet contains {stonks.size()} tickers")
+
+    good_tickers = ["NVDA", "MSI", "RXT", "V", "O"]
+    bad_tickers = ["banana", "nvda", "1", "TORC"]
+
+    for ticker in good_tickers:
+        print(f"Is {ticker} in StonkSet? {stonks.contains(ticker)}")
+    for ticker in bad_tickers:
+        print(f"Is {ticker} in StonkSet? {stonks.contains(ticker)}")
